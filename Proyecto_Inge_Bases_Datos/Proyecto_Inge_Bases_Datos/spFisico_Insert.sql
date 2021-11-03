99a47436685e66bcdf07228339c052da8f6371da
joalva18 <50782609+joalva18@users.noreply.github.com>
2021-11-03 09:49:01 -0600

    Add files via upload

---
+++ Proyecto_Inge_Bases_Datos/Proyecto_Inge_Bases_Datos/spFisico_Insert.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [dbo].[spFisico_Insert]
+
+
+    @Nombre NVARCHAR(50),
+    @CorreoCliente NVARCHAR(50),
+    @PrecioEstimado FLOAT,
+    @Condicion NCHAR(50),
+    @Descripcion TEXT,
+    @ProductoImagen1 IMAGE,
+    @ProductoImagen2 IMAGE,
+    @ProductoImagen3 IMAGE,
+    @Publicado BIT,
+    @FechaRegistrado DATETIME
+
+AS
+
+BEGIN
+    DECLARE @ProductoFisicoID INT
+
+    INSERT INTO [Producto] ([Nombre], [CorreoCliente], [PrecioEstimado], [Condicion], [Descripcion], [ProductoImagen1], [ProductoImagen2], [ProductoImagen3], [Publicado], [FechaRegistrado])
+            VALUES(@Nombre, @CorreoCliente, @PrecioEstimado, @Condicion, @Descripcion, @ProductoImagen1, @ProductoImagen2, @ProductoImagen3, @Publicado, @FechaRegistrado)
+
+     SET @ProductoFisicoID = (
+        SELECT MAX(ProductoID)
+        FROM Producto
+      )
+
+    INSERT INTO [Fisico] ([ProductoID], [CorreoCliente])
+        VALUES(@ProductoFisicoID, @CorreoCliente)
+END
---
+++ Proyecto_Inge_Bases_Datos/Proyecto_Inge_Bases_Datos/spVirtual_Insert.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE [dbo].[spVirtual_Insert]
+
+
+    @Nombre NVARCHAR(50),
+    @CorreoCliente NVARCHAR(50),
+    @PrecioEstimado FLOAT,
+    @Condicion NCHAR(50),
+    @Descripcion TEXT,
+    @ProductoImagen1 IMAGE,
+    @ProductoImagen2 IMAGE,
+    @ProductoImagen3 IMAGE,
+    @Publicado BIT,
+    @FechaRegistrado DATETIME,
+
+
+    --  Caracteristicas del virtual
+    @TipoDeArchivo NVARCHAR(50),
+    @DerechosDeProducto NVARCHAR(50) NULL,
+    @Fuentes NVARCHAR(50) NULL,
+    @FechaExpiracion DATETIME NULL
+
+AS
+BEGIN
+        DECLARE @ProductoVirtualID INT
+
+        INSERT INTO [Producto] ([Nombre], [CorreoCliente], [PrecioEstimado], [Condicion], [Descripcion], [ProductoImagen1], [ProductoImagen2], [ProductoImagen3], [Publicado], [FechaRegistrado])
+            VALUES(@Nombre, @CorreoCliente, @PrecioEstimado, @Condicion, @Descripcion, @ProductoImagen1, @ProductoImagen2, @ProductoImagen3, @Publicado, @FechaRegistrado)
+
+        SET @ProductoVirtualID = (
+            SELECT MAX(ProductoID)
+            FROM Producto
+        )
+
+    INSERT INTO [Virtual] ([ProductoID], [CorreoCliente], [TipoDeArchivo], [DerechosDeProducto], [Fuentes], [FechaExpiracion])
+        VALUES(@ProductoVirtualID, @CorreoCliente, @TipoDeArchivo, @DerechosDeProducto, @Fuentes, @FechaExpiracion)
+END
